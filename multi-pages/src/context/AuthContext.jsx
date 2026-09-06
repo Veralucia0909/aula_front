03a26abf59a07870156c42fadc7677d0dca1b1a9
Veralucia0909 <verinha539@hotmail.com>
2026-09-06 11:50:36 -0300

    links no header, consumindo dados do contexto, blog

--- multi-pages/src/context/AuthContext.jsx
+++ multi-pages/src/context/AuthContext.jsx
@@ -16,7 +16,7 @@ export const AuthProvider = ({children})=>{
 
 
 
-    return(
+   return(
         <AuthContext.Provider value={{user,login,logout}}>
             {children}
         </AuthContext.Provider>
